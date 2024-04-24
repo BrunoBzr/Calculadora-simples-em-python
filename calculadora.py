2f53f98c46d2df95c4cc5ec22153551545e3c554
Bruno Pereira <brunopereirabzrdev@gmail.com>
2024-04-24 12:49:36 -0300

    Criando as funções

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,10 @@
 import os
+import funcoes
 
 # \n - Tecla da barra invertida não funciona.
 
 while True:
-    os.system('clear')
+    os.system('cls')
     print('---------- CALCULADORA SIMPLES ----------')
     print('1 - Adição')
     print('2 - Subtração')
@@ -12,4 +13,16 @@ while True:
     print('0 - Sair')
     op = int(input('>> '))
 
+    if op == 0:
+        break
+    elif op == 1:
+        os.system('cls')
+        print('---------- ADIÇÃO ----------\n')
+        numero1 = float(input('Informe o primeiro valor: '))
+        numero2 = float(input('Informe o segundo valor: '))
+
+        resultado = funcoes.somar(numero1, numero2)
+
+        print(f'\nA soma de {numero1} + {numero2} é igual a {resultado}\n')
+        os.system('pause')
     print(op)
---
+++ funcoes.py
@@ -0,0 +1,3 @@
+def somar(n1, n2):
+    soma = n1 + n2
+    return soma
