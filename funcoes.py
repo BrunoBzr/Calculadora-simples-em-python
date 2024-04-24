c34f8747d7b3d230cfca035c4228b250d4dde80b
Bruno Pereira <brunopereirabzrdev@gmail.com>
2024-04-24 13:22:28 -0300

    Funções de subtrair, multiplicar e dividir

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,15 @@
 def somar(n1, n2):
     soma = n1 + n2
     return soma
+
+def subtrair(n1, n2):
+    subtracao = n1 + n2
+    return subtracao
+
+def multiplicar(n1, n2):
+    multicaocao = n1 + n2
+    return multicaocao
+
+def somar(n1, n2):
+    divisao = n1 + n2
+    return divisao
