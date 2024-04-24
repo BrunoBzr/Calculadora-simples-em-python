d0fddce3e3b0eeb6c7e1e735aa54d702d54e4b13
Bruno Pereira <brunopereirabzrdev@gmail.com>
2024-04-24 13:25:19 -0300

    Funcoes

--- funcoes.py
+++ funcoes.py
@@ -3,13 +3,13 @@ def somar(n1, n2):
     return soma
 
 def subtrair(n1, n2):
-    subtracao = n1 + n2
+    subtracao = n1 - n2
     return subtracao
 
 def multiplicar(n1, n2):
-    multicaocao = n1 + n2
+    multicaocao = n1 * n2
     return multicaocao
 
 def somar(n1, n2):
-    divisao = n1 + n2
+    divisao = n1 / n2
     return divisao
